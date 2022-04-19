1e8bca0e1592e24cae93abd8bbc8c8908a7787d0
zodac <arouge110@msn.com>
2022-04-20 09:46:35 +1200

    Docs update to unrar script for transmission

--- docker/transmission/scripts/unrar.sh
+++ docker/transmission/scripts/unrar.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # A simple script to extract a rar file inside a directory downloaded by Transmission
-find /"${TR_TORRENT_DIR}"/"${TR_TORRENT_NAME}" -name "*.rar" -execdir unrar e -o- "{}" \;
+# It uses environment variables passed by the transmission client to find and extract any rar files from a downloaded torrent into the folder they were found in
+# https://techblog.jeppson.org/2016/11/automatically-extract-rar-files-downloaded-transmission/
+find "/${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" -name "*.rar" -execdir unrar e -o- "{}" \;

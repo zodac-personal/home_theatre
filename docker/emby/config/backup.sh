1c95fad1c18893c020c43594fac70be666617a71
zodac <arouge110@msn.com>
2022-12-16 13:50:48 +1300

    Adding PiWiGo and MariaDB

--- docker/emby/config/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting Emby server backup"
-tar \
-  --exclude='*/cache' \
-  --exclude='*/logs' \
-  --exclude='*/metadata' \
-  -zcf /backups/emby.tar.gz /config/
-echo "Completed Emby server backup"

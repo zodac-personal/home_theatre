ad1324be05787a4f438d4ee2015565d64e6d78f2
zodac <arouge110@msn.com>
2023-01-16 17:08:46 +1300

    Removing unrar.sh script from Transmission

--- docker/transmission/config/unrar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# A simple script to extract a rar file inside a directory downloaded by Transmission
-# It uses environment variables passed by the transmission client to find and extract any rar files from a downloaded torrent into the folder they were found in
-# https://techblog.jeppson.org/2016/11/automatically-extract-rar-files-downloaded-transmission/
-find "/${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" -name "*.rar" -execdir unrar e -o- "{}" \;
